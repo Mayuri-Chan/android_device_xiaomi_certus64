4f6fc6c8fd9773f94b4a0c201f9b81bbccdee12f
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    certus: rro_overlays: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- device.mk
+++ device.mk
@@ -111,7 +111,8 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
+    TetheringConfigOverlay \
+    WifiOverlay
 
 # Call proprietary blob setup
 $(call inherit-product-if-exists, vendor/xiaomi/certus64/certus64-vendor.mk)
