76d2ddb6671e5c6695b5573aae01ced59f41332f
Kshitij Gupta <kshitijgm@gmail.com>
2020-11-01 20:33:25 +0100

    mt6765-common: Use POSP apns config - LineageOS one lacks of IMS support for a lot of carriers
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -93,6 +93,7 @@ TARGET_LD_SHIM_LIBS := /system/lib/libshowlogo.so|libshim_showlogo.so
 BOARD_VNDK_VERSION  := current
 PRODUCT_EXTRA_VNDK_VERSIONS := 28
 TARGET_COPY_OUT_VENDOR := vendor
+TARGET_COPY_OUT_PRODUCT := system/product
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
