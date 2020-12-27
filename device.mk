dfbed7da9f84641f987058e323e164ce5c09314a
Pierre-Hugues Husson <phh@phh.me>
2020-12-27 09:02:49 +0000

    certus: Add overlay for R IMS
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
+# IMS
+PRODUCT_PACKAGES += \
+    mtk-ims-telephony
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
