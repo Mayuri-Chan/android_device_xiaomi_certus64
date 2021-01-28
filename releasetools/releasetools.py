65b15ad37db6a671f21a7851d326f2f9f5659249
wulan17 <galihgustip@gmail.com>
2021-01-29 02:31:37 +0700

    Revert "mt6765-common: releasetools: Ensure userdata is mounted last"
    This reverts commit 879b57e9b6d8eba29003cffb4c966c5d62b8c46f.
    
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -3,9 +3,11 @@ import re
 
 def FullOTA_InstallEnd(info):
     OTA_InstallEnd(info)
+    return
 
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
+  return
 
 def AddImage(info, basename, dest):
   name = basename
@@ -14,13 +16,7 @@ def AddImage(info, basename, dest):
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
-  PatchVendor(info)
   info.script.Print("Patching firmware images...")
   AddImage(info, "vbmeta.img", "/dev/block/platform/bootdevice/by-name/vbmeta")
+  return
 
-def PatchVendor(info):
-  info.script.Print("Patching vendor init scripts...")
-  info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/bootdevice/by-name/vendor", "/vendor");')
-  info.script.AppendExtra('run_program("/sbin/sed", "-i", "s/wait,check,quota/latemount,wait,check,quota/", "/vendor/etc/fstab.mt6765");')
-  info.script.AppendExtra('run_program("/sbin/sed", "-i", "s/fstab.mt6765$/fstab.mt6765 --early\\n    mount_all \/vendor\/etc\/fstab.mt6765 --late/", "/vendor/etc/init/hw/init.mt6765.rc");')
-  info.script.AppendExtra('unmount("/vendor");')
