138dbb1bdc028de163cc7eacc84ab5399eebfd08
YumeMichi <do4suki@gmail.com>
2021-09-12 20:50:34 +0200

    certus: Build binderized Thermal HIDL HAL
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I3c198ca5258ad72794e0a4b558f2f8ffc533d8aa

--- device.mk
+++ device.mk
@@ -212,6 +212,10 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/thermal/.ht120.mtc:$(TARGET_COPY_OUT_VENDOR)/etc/.tp/.ht120.mtc \
     $(DEVICE_PATH)/configs/thermal/thermal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/.tp/thermal.conf \
