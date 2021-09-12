b95173dc584788657271aabb6fa43204f5650dea
Jan Dula <frantisheq@gmail.com>
2021-09-12 20:20:29 +0200

    certus: Build Snap camera
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I7eb6d24276248ffce085b9da9b351f9dc5cd5bd9

--- device.mk
+++ device.mk
@@ -52,6 +52,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 # DRM
 PRODUCT_PACKAGES += \
     libmockdrmcryptoplugin \
