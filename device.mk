7e16c0e48502084a896e0f64cd036247978d61e5
Tri Vo <trong@google.com>
2021-09-12 19:47:28 +0200

    certus: Build libmockdrmcryptoplugin
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I05ca9307834f5d30cadd0984da970ac1dbec4f96

--- device.mk
+++ device.mk
@@ -52,6 +52,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    libmockdrmcryptoplugin
+
 # Dynamic partitions
 PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
