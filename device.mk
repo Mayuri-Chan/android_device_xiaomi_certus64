ed79b30c729d55c41cfa15ca23b9c6629cb872d6
Prashant Malani <pmalani@google.com>
2021-09-12 20:53:12 +0200

    certus: Build binderized Vibrator HIDL HAL
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ib40fced9b39414b1b11f54c1f7940cbe592a54eb

--- device.mk
+++ device.mk
@@ -228,6 +228,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     hostapd \
