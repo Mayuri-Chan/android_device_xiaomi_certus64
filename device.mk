f5f59684aac07b8ce42c2f96b4052643a37421e7
wulan17 <galihgustip@gmail.com>
2021-09-26 14:48:33 +0000

    certus64: Add more missing permissions
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- device.mk
+++ device.mk
@@ -101,7 +101,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-imsinit.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-imsinit.xml \
-    $(LOCAL_PATH)/permissions/privapp-permissions-certus.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-certus.xml
+    $(LOCAL_PATH)/permissions/product-privapp-permissions-certus.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-certus.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-certus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-certus.xml
 
 # RCS
 PRODUCT_PACKAGES += \
